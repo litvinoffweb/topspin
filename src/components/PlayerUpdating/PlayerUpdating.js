120b4385628de28bea8323c0e2fb4e5f2db688a2
Stanislav Litvinov <slitvinov@endava.com>
2018-05-15 13:37:46 +0300

    change style in update form

--- src/components/PlayerUpdating/PlayerUpdating.js
+++ src/components/PlayerUpdating/PlayerUpdating.js
@@ -51,31 +51,52 @@ class PlayerUpdating  extends Component {
     render(){
 
         
-        const { player: { Age, Name, Surname, id, facebookID, Rating, Style, chosen}, handleUpdateCancel} = this.props;
+        const { player: { Age, Name, Surname, id, facebookID, Rating, Style, chosen},userID, handleUpdateCancel, match, location} = this.props;
         return(
              <tr>
-                <td>
-                <Form >
-                            <table>
-                                <tbody>
-                                <tr><td><span className='td-span-float-left'>Name: </span></td><td><input className='input-main' onChange={(e) => this.handleChange(e)} style={{width: '100%', padding: '0px', marginBottom: '0px'}}  name="Name" type='text' id="name" placeholder='Name:' defaultValue={Name}/></td></tr>
-                                <tr><td><span className='td-span-float-left'>Surname: </span></td><td><input className='input-main' onChange={(e) => this.handleChange(e)} style={{width: '100%', padding: '0px', marginBottom: '0px'}} name="Surname" type='text' id="surname" placeholder='Surname:' defaultValue={Surname}/></td></tr>
-                                <tr><td><span className='td-span-float-left'>Age: </span></td><td><input className='input-main' onChange={(e) => this.handleChange(e)} style={{width: '100%', padding: '0px', marginBottom: '0px'}} name="Age" type='text' id="age" placeholder='Age:'  defaultValue={Age}/></td></tr>
-                                <tr><td><span className='td-span-float-left'>Rating: </span></td><td><input className='input-main' onChange={(e) => this.handleChange(e)} style={{width: '100%', padding: '0px', marginBottom: '0px'}} name="Rating" type='text' id="rating" placeholder='Rating:'  defaultValue={Rating}/></td></tr>
-                                <tr><td><Button  className='button-update' style={{marginTop: '0px', marginBottom: '0px', width: '70px'}} onClick={ () => this.handleUpdate(id)}>
-                                            UPDATE
-                                        </Button>
-                                    </td>
-                                    <td>
-                                    <Button  className='button-update' style={{marginTop: '0px', marginBottom: '0px', width:'70px'}} onClick={ () => handleUpdateCancel(id)}>
-                                            CANCEL
-                                        </Button>
-                                    </td>
-                                </tr>
-                                </tbody>
-                            </table>
-                        </Form>
+                 
+                <td colspan='2'>
+                    
+                                    <Form >
+                                     <table>
+                                            <tbody>
+                                            <tr><td><span className='td-span-float-left'>Name: </span></td>
+                                                <td><input className='input-main' onChange={(e) => this.handleChange(e)} style={{width: '100%', padding: '0px', marginBottom: '0px'}}  name="Name" type='text' id="name" placeholder='Name:' defaultValue={Name}/></td>
+                                                
+                                            </tr>
+                                            <tr><td><span className='td-span-float-left'>Surname: </span></td>
+                                                <td><input className='input-main' onChange={(e) => this.handleChange(e)} style={{width: '100%', padding: '0px', marginBottom: '0px'}} name="Surname" type='text' id="surname" placeholder='Surname:' defaultValue={Surname}/></td></tr>
+                                            <tr><td><span className='td-span-float-left'>Age: </span></td>
+                                                <td><input className='input-main' onChange={(e) => this.handleChange(e)} style={{width: '100%', padding: '0px', marginBottom: '0px'}} name="Age" type='text' id="age" placeholder='Age:'  defaultValue={Age}/></td></tr>
+                                            <tr><td><span className='td-span-float-left'>Rating: </span></td>
+                                                <td><input className='input-main' onChange={(e) => this.handleChange(e)} style={{width: '100%', padding: '0px', marginBottom: '0px'}} name="Rating" type='text' id="rating" placeholder='Rating:'  defaultValue={Rating}/></td></tr>
+                                            <tr><td/><td><Button label='UPDATE' className='button-update' style={{marginTop: '0px', marginBottom: '0px', width: '100%'}} onClick={ () => this.handleUpdate(id)}>
+                                                      
+                                                    </Button>
+                                                </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </Form>
                    </td>
+                   <td colSpan='3'>
+                        <table>
+                            <tbody>
+                                <tr><td colSpan='2'><img src={`https://graph.facebook.com/${facebookID}/picture?type=small`} className='avatar-facebook' alt='\' /></td></tr>
+                                <tr><td ><span className='td-span-float-left'>Name:</span></td><td>{this.state.Name}</td></tr>
+                                <tr><td ><span className='td-span-float-left'>Surname:</span></td><td>{this.state.Surname}</td></tr>
+                                <tr><td ><span className='td-span-float-left'>Age:</span></td><td>{this.state.Age}</td></tr>
+                                <tr><td ><span className='td-span-float-left'>Rating:</span></td><td>{this.state.Rating}</td></tr>
+                                
+                            </tbody>
+                        </table>
+                    </td>
+                    <td colSpan={(userID === 'YK4O4xkCEtcwBIdwyRVVzuFCbzH3' && location.pathname === `/admin/create_player`) ? '2' : '3'} style={{position: 'relative'}}>
+                        <Button  className='button-cancel' label='CANCEL' style={{marginTop: '0px', marginBottom: '0px', position: 'absolute', width: '50%', top:'10px', right: '10px'}} onClick={ () => handleUpdateCancel(id)}>
+                            
+                         </Button>                   
+                    </td>
+                   
                 </tr>
         )
     }
@@ -83,7 +104,7 @@ class PlayerUpdating  extends Component {
 }
 
 const mapStateToProps = state => ({
-
+    userID: firebase.auth.uid
 })
 
 const mapDispatchToProps = dispatch => ({
